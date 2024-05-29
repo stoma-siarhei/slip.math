9bb6dfd7fd3d4a89f784593739b44bce5dd15709
scander <scander@yandex.ru>
2024-05-29 11:44:08 +0800

    #2 - Убрано использование предварительно откомпилированных файлов

--- combination/combination.cpp
+++ combination/combination.cpp
@@ -1,10 +1,1 @@
-﻿// combination.cpp : Определяет функции для статической библиотеки.
-//
-
-#include "pch.h"
-#include "framework.h"
-
-// TODO: Это пример библиотечной функции.
-void fncombination()
-{
-}
+﻿
